3e91d5a9decec08635d6594bc9bdaaa91cf52c7e
DevEAL <desarrollo@enalgunlugarestudio.com>
2021-11-14 15:05:05 -0500

    Cambios Jon

--- PrincipioInversionDependencias.php
+++ PrincipioInversionDependencias.php
@@ -1,1 +1,27 @@
+<?php
+interface Conexion {
+    public function getDatos();
+    public function setDatos();
+}
 
+class DatabaseService implements Conexion {
+    public function getDatos() {}
+    public function setDatos() {}
+}
+
+class APIService implements Conexion {
+    public function getDatos() {}
+    public function setDatos() {}
+}
+
+class AccesoDatos {
+    private $conexion;
+
+    public function AccesoDatos($conexion) {
+        this->Conexion;
+    }
+
+    private function getDatos() {
+        $conexion->getDatos();
+    }
+}
