e15c39ed582868ccf7b2044c7a2f45840b8e3c36
josecast1994 <94152837+josecast1994@users.noreply.github.com>
2021-11-14 15:23:51 -0500

    Cambios leonardo

--- PrincipioResponsabilidadUnica.php
+++ PrincipioResponsabilidadUnica.php
@@ -1,1 +1,18 @@
 
+<?php
+class Coche {  
+    $marca;
+
+    function Coche(marca){ $this->marca = marca; }
+
+    function getMarcaCoche(){ return marca; }
+}
+
+class CocheDB{  
+    function guardarCocheDB(Coche ){ ... }
+    function eliminarCocheDB(Coche ){ ... }
+} 
+
+
+
+?>
